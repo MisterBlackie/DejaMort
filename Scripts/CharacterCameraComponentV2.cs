4a7a1e4830fc764e488c93f1e6ffea8612475b62
MisterBlackie <alexrevenger234@gmail.com>
2019-09-19 09:49:59 -0400

    Ajout character movement v2
    Fonctionne pas encore, v1 a utiliser

---
+++ Scripts/CharacterCameraComponentV2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CharacterCameraComponent : MonoBehaviour
+{
+    public float CameraSpeed = 2f;
+    private float maxAngle = 60f;
+    private float angle;
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        angle += -Input.GetAxis("Mouse Y") * Time.deltaTime * CameraSpeed;
+        angle = Mathf.Clamp(angle, -maxAngle, maxAngle);
+        transform.Rotate(-Input.GetAxis("Mouse Y") * Time.deltaTime * CameraSpeed, 0, 0);
+        /*transform.eulerAngles.y = Mathf.Clamp(transform.eulerAngles.z, -maxAngle, maxAngle);*/
+    }
+}
