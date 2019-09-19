85df7ce9df7b12c11db846a98ec96a0f19a8c645
MisterBlackie <alexrevenger234@hotmail.com>
2019-09-19 00:56:02 -0400

    Ajout characterMoving

---
+++ Scripts/CharacterCameraComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CharacterCameraComponent : MonoBehaviour
+{
+    public float CameraSpeed = 100f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(Input.GetAxis("Mouse Y") * Time.deltaTime * CameraSpeed, 0, 0);
+    }
+}
