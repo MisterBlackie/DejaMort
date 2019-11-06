1c4dc62b50a19c9a21004a252f19de9c3a6fa1d8
MisterBlackie <alexrevenger234@hotmail.com>
2019-11-06 00:15:39 -0500

    Ajout de la classe succès
    Ajout d'un classe qui représente les succès.
    AchievementType représente les types de succès possible
    
    Ajout du callback à l'event onDeath pour avancer les progrès du type mort

---
+++ Scripts/Success/Achievement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public enum AchievementType {
+    Jump,
+    Attack,
+    Die,
+}
+
+[System.Serializable]
+public class Achievement
+{
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public int Progress { get; private set; }
+    public int ProgressNeeded { get; private set; }
+    public int ProgressLeft => !Unlocked ? ProgressNeeded - Progress : 0;
+    public DateTime? WonDate { get; private set; }
+    public AchievementType Type { get; private set; }
+    public bool Unlocked => Progress >= ProgressNeeded;
+
+    public void AddProgress() {
+        if (Unlocked)
+            return;
+
+        Progress++;
+        if (Unlocked)
+            onUnlock?.Invoke(this, new AchievementEventArgs(this));
+    }
+
+    event EventHandler<AchievementEventArgs> onUnlock;
+
+    public Achievement(string name, string description, int progressNeeded, AchievementType type, int progressDone = 0, DateTime? dateWon = null) {
+        Name = name;
+        Description = description;
+        ProgressNeeded = progressNeeded;
+        Progress = progressDone;
+        WonDate = dateWon;
+        Type = type;
+    }
+}
+
+public class AchievementEventArgs {
+    public Achievement achievement { get; set; }
+
+    public AchievementEventArgs(Achievement acv)
+    {
+        achievement = acv;
+    }
+}
---
+++ Scripts/Success/AchievementManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class AchievementManager : MonoBehaviour
+{
+    [SerializeField]
+    List<Achievement> achievements;
+    
+    public void RegisterEvent(AchievementType type) {
+        foreach (var achievement in achievements.Where(a => a.Type == type && !a.Unlocked)) {
+            achievement.AddProgress();
+        }
+    }
+
+    private void LoadAchievementsFromDatabase() {
+
+    }
+
+    private void SaveAchievementsToDatabase() {
+
+    }
+
+    void Start()
+    {
+        LoadAchievementsFromDatabase();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveAchievementsToDatabase();
+    }
+}
